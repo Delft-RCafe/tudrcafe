61a1f0b244f038e398dfe3414ed8c7a0bd470450
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-01-11 13:01:08 +0100

    initial commit

---
+++ R/sessions.R
@@ -0,0 +1,22 @@
+
+#' @export
+upcoming_session <- function(url = "https://delft-rcafe.github.io/home", session_no = 1){
+
+  session_no_conv <- session_no + 1
+  upcoming <- url |>
+    rvest::read_html() |>
+    rvest::html_element(paste0("div.callout:nth-child(", session_no_conv, ")")) |>
+    rvest::html_text2()
+
+  upcoming_tbl <- upcoming |>
+    tibble::as_tibble() |>
+    dplyr::mutate(
+      topic = stringr::str_extract(value, ".*(?=\\n)"),
+      date = stringr::str_extract(value, "(?<=Date: ).*"),
+      time = stringr::str_extract(value, "(?<=Time: ).*"),
+      location = stringr::str_extract(value, "(?<=Place: ).*")
+    ) |>
+    dplyr::select(!value)
+
+  return(upcoming_tbl)
+}
